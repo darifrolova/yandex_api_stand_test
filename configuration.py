6f0cc5d5600c0ee7072b13ffcf32ea60fb1dbe26
Дарья Фролова <darifrolova@MacBook-Air-Dara.local>
2023-12-12 11:03:20 +1000

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://bf226d24-dc4b-493f-8126-e62fa86ee184.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
